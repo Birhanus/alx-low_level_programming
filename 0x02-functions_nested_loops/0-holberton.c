ca8055863958feec3cb1a1ed7c09da552bc5a29a
birhanus <birhanushimles716@gmail.com>
2021-08-31 02:20:32 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ *always return zero
+ *
+ */
+int main(void)
+{
+	char h = 'H', o = 'o', l = 'l', b = 'b', e = 'e', r = 'r', t = 't', n = 'n';
+	putchar(h);
+	putchar(o);
+	putchar(l);
+	putchar(b);
+	putchar(e);
+	putchar(r);
+	putchar(t);
+	putchar(n);
+	putchar("\n");
+	return (0);
+
+}
