361cd77879c24edcc1343e35f0e9f1124836571a
birhanus <birhanushimles716@gmail.com>
2021-08-31 06:28:45 -0700

    lower case

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,13 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-/*
+/**
  *_putchar writes the character a to stdout
  *@c: the character to print
  */
 int _putchar(char a);
 #endif
+/**
+ * print_alphabet - print lowercase alphabet
+ */
+
+void print_alphabet(void);
