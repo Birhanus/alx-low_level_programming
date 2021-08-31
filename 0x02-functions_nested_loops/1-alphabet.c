b1b15320b081e59b9ad4928aaa768d2a30570b0a
birhanus <birhanushimles716@gmail.com>
2021-08-31 06:13:48 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print alphabet in lower case
+ *Return o
+ *
+ */
+int main(void)
+{
+	char a = 'a';
+	for (char a = 'a';a <= 'z';a++)
+	{
+		_putchar(a);
+	}
+	putchar('\n');
+}
