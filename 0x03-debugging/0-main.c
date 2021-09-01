370f8979de9df59c5a241170bf27e478856aec9b
birhanus <birhanushimles716@gmail.com>
2021-09-01 09:19:38 -0700

    postive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,28 +3,14 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *positive_or_negative-function to check number postive or negative
+ *main-function to check number postive or negative
  *Return:0
  */
-int positive_or_negative(void)
+int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-	{
-		printf("%i is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%i is zero\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%i is negative\n", n);
-	}
-
+	n = 0;
+	positive_or_negative(n);
 	return (0);
 }
