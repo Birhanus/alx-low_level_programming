42b3d9f3f1f3a4425c90cd26d6942e3ed91e085f
birhanus <birhanushimles716@gmail.com>
2021-09-02 08:30:11 -0700

    check upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main"
+
+/**
+ *_isupper-function check uppercasw
+ *@c: Character to check
+ *Return:1 if upper case, 0 if not
+ */
+int _isupper(char c)
+{
+	if (c >= '65' && c <= '90')
+		_putchar('1');
+	else
+		_putchar('0');
+
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+ *_isupper-function to check uppercase
+ *Return:1 if upper case,0 if not upper case
+ */
+int _isupper(char);
+#endif
