b7ac85ee6746a212bc5e7d1964a7e6c92f9430a7
birhanus <birhanushimles716@gmail.com>
2021-09-02 08:40:59 -0700

    upper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,6 +4,17 @@
 /**
  *_isupper-function to check uppercase
  *Return:1 if upper case,0 if not upper case
+ */void _isupper(char);
+/**
+ *_putchar writes the character a to stdout
+ *@c: the character to print
  */
-int _isupper(char);
+
+/**
+ *_putchar writes the character a to stdout
+ *@c: the character to print
+ */
+int _putchar(char a);
+
+
 #endif
