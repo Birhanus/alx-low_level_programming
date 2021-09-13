77db60f9340ba964ad3bac024d078ef88c36debe
birhanus <birhanushimles716@gmail.com>
2021-09-13 10:55:07 -0700

    arrays and pointers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strchr - search a char in a string
+ * @s: pointer to string
+ * @c: char to search
+ * Return: Pointer to first find
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+
+		}
+		else
+			s++;
+	}
+	if (*s == c)
+		return (s);
+	else
+		return (0);
+}
+© 2021 GitHub, Inc.
