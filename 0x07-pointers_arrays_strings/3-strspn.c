1f7bf82eeeda54e1460d8fbb19dbd0dcc827aa22
birhanus <birhanushimles716@gmail.com>
2021-09-13 11:02:10 -0700

    arrays and pointers

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "mainn.h"
+
+/**
+ * _strspn - lenght of a prefix
+ * @s: array
+ * @accept: number to accept
+ * Return: number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (j = 0; *(s + j); j++)
+	{
+		for (i = 0; *(accept + i); i++)
+		{
+			if (*(s + j) == *(accept + i))
+				break;
+		}
+	if (*(accept + i) == '\0')
+		break;
+	}
+	return (j);
+}
