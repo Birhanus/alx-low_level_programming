2b85385c032c84404442d0fcae02ed40de47e0d8
birhanus <birhanushimles716@gmail.com>
2021-09-15 15:08:53 -0400

    string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,14 @@
 #include "main.h"
 /**
- *_strlen_recursion- print length 
- * @s: string
- */
+* _strlen_recursion - count lenght
+* @s: string
+* Return: size;
+*/
 
 int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
-	{
-		int n;
-		
-		_strlen_recursion(s + 1);
-		n++;
-		return (n);
-	}
+		return (1 + _strlen_recursion(s + 1));
+
+	return (0);
 }
