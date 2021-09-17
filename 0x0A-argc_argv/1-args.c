00c482e20d340c0dc136a6bfe9a6a462eeebb30d
birhanus <birhanushimles716@gmail.com>
2021-09-17 11:06:21 -0400

    main argument

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: array of strings
  * Return: int
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
