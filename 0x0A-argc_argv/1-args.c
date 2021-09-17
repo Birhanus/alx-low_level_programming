0e7ef153d1b994769c6c838434772206060126e6
birhanus <birhanushimles716@gmail.com>
2021-09-17 07:37:52 -0400

    count argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints program name
+ * @argc: argument count
+ * @argv: array of strings
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	 printf("%d\n",argc-1);
+}
