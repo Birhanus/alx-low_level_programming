524d95d5f8701a56c1224fe0f377e92d0a593df4
birhanus <birhanushimles716@gmail.com>
2021-09-21 14:41:27 -0400

    C - malloc, free

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * str_concat - check if a char is a digit
+ * @s1: the first string
+ * @s2: the second string
+ * Return: the concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *a;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int i2 = 0;
+	unsigned int j2 = 0;
+
+	while (s1 != NULL && s1[i])
+		i++;
+	while (s2 != NULL && s2[j])
+		j++;
+	a = malloc((i + j) * sizeof(char) + 1);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	while (i2 < i)
+	{
+		a[i2] = s1[i2];
+		i2++;
+	}
+
+	while (j2 < j)
+	{
+		a[j2 + i2] = s2[j2];
+		j2++;
+	}
+	return (a);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*free_grid - free the matrix
+*@grid: the pointer
+*@height: the height
+*Return: Liberation of the space comrades
+*/
+void free_grid(int **grid, int height)
+{
+	int cont;
+
+	for (cont = 0; cont < height; cont++)
+		free(grid[cont]);
+	free(grid);
+}
