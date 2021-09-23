e4080a48d9f6ee52f18c702ccbc3d496220425d7
birhanus <birhanushimles716@gmail.com>
2021-09-23 15:11:41 -0400

    more malloc and free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - Fuction
+ * @b: int
+ *
+ * Return: test
+**/
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
