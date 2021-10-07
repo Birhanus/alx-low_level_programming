63a2a54f43452c2a53981eb8269c05759cbb5aba
birhanus <birhanushimles716@gmail.com>
2021-10-07 12:51:10 -0700

    excute before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void myStartupFun (void) __attribute__ ((constructor));
+/**
+ *myStartupFun - excute before main function
+ *Return: Nothing
+ *
+ *
+ */
+void myStartupFun (void)
+{
+    printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
