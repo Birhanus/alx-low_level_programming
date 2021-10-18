0f9f20366a51b00ff5a81d0f7a81e329b55f210a
birhanus <birhanushimles716@gmail.com>
2021-10-18 15:20:59 -0700

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,55 +1,45 @@
-#include main.h"
+#include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 /**
- * _strlen - find length of string
- * @str: string
- * Return: length
+ * _strlen - finds the length of a string
+ * @str: pointer to the string
+ *
+ * Return: length of the string
  */
-int _strlen(char *str)
+size_t _strlen(char *str)
 {
-	int len;
+	size_t i;
 
-	for (len = 0; str[len] != '\0'; len++)
+	for (i = 0; str[i]; i++)
 		;
-
-	return (len);
+	return (i);
 }
 
 /**
- * create_file - creates file with permissions rw------- and writes content in
- * if file already exists, don't change permissions and just truncate it
- * @filename: name to give to new file
- * @text_content: writes this content into file
- * Return: 1 on success, -1 on error
+ * create_file - creates a file.
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int n_wrote;
+	ssize_t len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-
-	/* create with permissions if file doesn't exist, else truncate */
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-
-	/* if nothing to write, just return newly created file */
-	if (!text_content)
-	{
-		close(fd);
-		return (1);
-	}
-
-	/* write */
-	n_wrote = write(fd, text_content, _strlen(text_content));
-	if (n_wrote == -1 || n_wrote != _strlen(text_content))
-	{
-		close(fd);
-		return (-1);
-	}
-
+	if (text_content != NULL)
+		len = write(fd, text_content, _strlen(text_content));
 	close(fd);
+	if (len == -1)
+		return (-1);
 	return (1);
 }
