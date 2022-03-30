a3dde20227361d3c0333a3104ad752955eba9fbc
birhanus <birhanushimles716@gmail.com>
2022-03-30 07:38:46 +0000

    add proto type

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,10 @@
 #ifndef __search_algos__h__
 #define __search_algos__h__
 
+#include <math.h>
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
+int jump_search(int *array, size_t size, int value);
 #endif
