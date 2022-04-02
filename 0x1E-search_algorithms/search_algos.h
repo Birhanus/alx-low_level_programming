4ba6ba2971b04e72c053f1eb2247a2f77e3e171c
birhanus <birhanushimles716@gmail.com>
2022-04-02 18:26:45 +0000

    fix conflict

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,11 +3,11 @@
 
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t left, size_t right, int value)
 #endif
